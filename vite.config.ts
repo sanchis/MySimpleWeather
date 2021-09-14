bd737f9db3234d0caff16909e2355f54bb632cb0
sanchis <sanchis1992@gmail.com>
2021-09-14 18:35:21 +0200

    fix: config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(),tsconfigPaths()],
-  base: 'https://weather.asanchis.dev'
+  base: 'https://weather.asanchis.dev/'
 })
