0180635128d064447ece7391aaa713e522cc9eea
sanchis <sanchis1992@gmail.com>
2021-09-14 20:11:23 +0200

    feat: test api

--- src/services/tiempo.ts
+++ src/services/tiempo.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { MunicipiosEndpointResponse, Tiempo } from './tiempo.model'
 
 export async function getMunicipios (): Promise<MunicipiosEndpointResponse[]> {
-  return await axios.get<MunicipiosEndpointResponse[]>('municipios')
+  return await axios.get<MunicipiosEndpointResponse[]>('maestro/municipios')
     .then(response => response.data)
     .then(data => data.map((municipio) => ({ ...municipio, CODIGOINE: municipio.CODIGOINE.substr(0, 5) })))
 }
