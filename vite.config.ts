d88ab69f592703fa6716b6c21fd03e96b7294fa2
sanchis <sanchis1992@gmail.com>
2021-09-14 18:34:17 +0200

    fix: base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(),tsconfigPaths()]
+  plugins: [reactRefresh(),tsconfigPaths()],
+  base: 'https://weather.asanchis.dev'
 })
