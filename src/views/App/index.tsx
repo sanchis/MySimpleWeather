0f25d53fe05d241c59a98098e1740a815ea16f5d
sanchis <sanchis1992@gmail.com>
2021-09-21 10:23:46 +0200

    feat: remove unnecesary components

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,17 +1,8 @@
 import React, { ReactElement } from 'react'
-import Detail from 'views/detail'
 import Index from 'views/index'
-import { Route, Switch } from 'wouter'
 
 export default function App (): ReactElement {
   return (
-    <>
-      <Switch>
-        <Route path='/' component={Index} />
-        <Route path='/t/:id'>
-          {(params) => <Detail id={params.id} />}
-        </Route>
-      </Switch>
-    </>
+    <Index />
   )
 }
